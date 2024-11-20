accd59a90e6954604e91ebf5ff28474819e5b1c8
Subhash Kumar <subhashkushwah134@gmail.com>
2024-11-20 19:56:38 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    outDir: 'dist',
-  },
-  server: {
-    open: true,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
